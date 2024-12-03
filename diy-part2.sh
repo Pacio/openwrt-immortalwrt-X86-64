51c588a6bc4769f9ec9a4cc1d3fb6794ef77df27
Pacio <4995125@163.com>
2024-12-03 22:49:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-# sed -i 's/eth1/eth3/g' package/base-files/files/etc/board.d/99-default_network
+sed -i 's/eth1/eth3/g' package/base-files/files/etc/board.d/99-default_network
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
