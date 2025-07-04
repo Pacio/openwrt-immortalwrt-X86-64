6b700943d156eeee68b4b7d3bbc7de69fe392472
Pacio <4995125@163.com>
2025-07-05 00:36:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,6 +9,15 @@
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 
+WORKINGDIR="`pwd`/feeds/packages/net/smartdns"
+mkdir $WORKINGDIR -p
+rm $WORKINGDIR/* -fr
+wget https://github.com/pymumu/openwrt-smartdns/archive/master.zip -O $WORKINGDIR/master.zip
+unzip $WORKINGDIR/master.zip -d $WORKINGDIR
+mv $WORKINGDIR/openwrt-smartdns-master/* $WORKINGDIR/
+rmdir $WORKINGDIR/openwrt-smartdns-master
+rm $WORKINGDIR/master.zip
+
 sed -i '1i src-git immpackage https://github.com/tudaole/OpenWrt-ImmortalWrt-Packages' feeds.conf.default
 ./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns && rm -rf feeds/luci/applications/luci-app-passwall && rm -rf feeds/luci/applications/luci-app-smartdns
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
