c843735714b035c474885925e6ccf7b48d17da71
Pacio <4995125@163.com>
2025-08-21 15:09:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,9 +13,3 @@
 # Modify default IP
 sed -i 's/eth1/eth3/g' package/base-files/files/etc/board.d/99-default_network
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
-
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
