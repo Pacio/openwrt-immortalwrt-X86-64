4565374afe8555c04da5e96c1b3e1814fb2775a9
Pacio <4995125@163.com>
2025-10-13 23:28:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,12 +10,6 @@
 # See /LICENSE for more information.
 #
 
-rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
-git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
-rm -rf feeds/luci/applications/luci-app-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
-
-
 # Modify default IP
 sed -i 's/eth1/eth3/g' package/base-files/files/etc/board.d/99-default_network
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
